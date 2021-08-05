bcbbffe9a187627b3d9e893049550c666483b589
Elyuha <frolov199918@mail.ru>
2021-08-05 12:56:08 +0300

    oauth2 start [oauth2]

--- src/main/java/com/javamaster/config/SecurityConfig.java
+++ src/main/java/com/javamaster/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.javamaster.config;
 
 import com.javamaster.config.jwt.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +15,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
